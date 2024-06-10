e3d1611712136f5e33bf0bda270d0fc0e22c3af6
Veeranji49 <veeranjijlkr49@gmail.com>
2024-06-10 13:21:34 +0530

    Spring-security-Jwt configuration

--- src/main/java/com/example/Config/SecurityConfig.java
+++ src/main/java/com/example/Config/SecurityConfig.java
@@ -1,77 +1,83 @@
 package com.example.Config;
 
+import com.example.Filter.JwtFilter;
+import com.example.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@CrossOrigin
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetails(PasswordEncoder encoder) {
+    @Autowired
+    private JwtFilter jwtFilter;
 
-        UserDetails admin = User.builder()
-                .username("anji")
-                .password(encoder.encode("anji@2001"))
-                .roles("ADMIN")
+    // Defines a UserDetailsService bean for user authentication
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
+
+
+    // Configures the security filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(Customizer.withDefaults()) // Apply CORS
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/addUser", "/auth/login")
+                        .permitAll()// Permit all requests to certain URLs
+                        .anyRequest().authenticated()) // Require authentication for all other requests
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Set session management to stateless
+                .authenticationProvider(authenticationProvider()) // Register the authentication provider
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) // Add the JWT filter before processing the request
                 .build();
-
-        UserDetails user = User.builder()
-                .username("veeru")
-                .password(encoder.encode("veeru@2001"))
-                .roles("USER")
-                .build();
-
-        InMemoryUserDetailsManager imdm = new InMemoryUserDetailsManager(admin, user);
-        return imdm;
     }
 
-   /* @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
-                .authorizeHttpRequests(req -> req
-                        .requestMatchers("/public/rest/*").permitAll()
-                        .requestMatchers("/rest/getone/{id}").hasRole("ADMIN")
-                        .requestMatchers("/rest/getall").hasAnyRole("ADMIN", "USER")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
-    }*/
 
+    // Creates a DaoAuthenticationProvider to handle user authentication
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(req -> req
-                        //.requestMatchers("/public/rest/*").permitAll()
-                        .requestMatchers("/rest/msg").permitAll()
-                        .requestMatchers("/rest/getall").hasRole( "USER")
-                        .requestMatchers("/rest/**").hasAnyRole("ADMIN","USER")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-        return http.build();
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
 
+    // Defines a PasswordEncoder bean that uses bcrypt hashing by default for password encoding
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+
+    // Defines an AuthenticationManager bean to manage authentication processes
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
